a426530f2b00a47d50cc483f24520525468e6ac4
DESKTOP-PL1SE9F\dhffk <choibh6258@hanyang.ac.kr>
2022-07-22 15:20:03 +0900

    [기능추가] QnA, Study, Project 애니메이션 추가
    QnA, Study, Project 애니메이션 추가

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{ useRef } from "react";
 import iconMBL from "../assets/img/MBL.png";
 import iconQ from "../assets/img/Q.png";
 import iconS from "../assets/img/S.png";
@@ -6,20 +6,92 @@ import iconP from "../assets/img/P.png";
 import icon_ from "../assets/img/_.png";
 
 function SideNav () {
+    const ref_title_QnA = useRef();
+    const ref_container_QnA = useRef();
+    const ref_title_Study = useRef();
+    const ref_container_Study = useRef();
+    const ref_title_Project = useRef();
+    const ref_container_Project = useRef();
+    const handleClassNameQ = () => {
+        const span = ref_title_QnA.current;
+        const span2 = ref_container_QnA.current;
+        if(span2.className ==="container expanded"){
+            span.className = "title";
+            span2.className = "container";
+        }else{
+            span.className += " expanded";
+            span2.className += " expanded";
+            ref_container_Study.current.className = "container";
+            ref_title_Study.current.className = "title";
+            ref_container_Project.current.className = "container";
+            ref_title_Project.current.className = "title";
+        }
+    }
+    const handleClassNameS = () => {
+        const span = ref_title_Study.current;
+        const span2 = ref_container_Study.current;
+        if(span2.className ==="container expanded"){
+            span.className = "title";
+            span2.className = "container";
+        }else{
+            span.className += " expanded";
+            span2.className += " expanded";
+            ref_container_QnA.current.className = "container";
+            ref_title_QnA.current.className = "title";
+            ref_container_Project.current.className = "container";
+            ref_title_Project.current.className = "title";
+        }
+    }
+    const handleClassNameP = () => {
+        const span = ref_title_Project.current;
+        const span2 = ref_container_Project.current;
+        if(span2.className ==="container expanded"){
+            span.className = "title";
+            span2.className = "container";
+        }else{
+            span.className += " expanded";
+            span2.className += " expanded";
+            ref_container_Study.current.className = "container";
+            ref_title_Study.current.className = "title";
+            ref_container_QnA.current.className = "container";
+            ref_title_QnA.current.className = "title";
+        }
+    }
+    
     return (
-        <div className="SideNav">
-            <nav>
-                <div className="nav_wrapper">
-                    <input type={"image"} name="MBL" src={iconMBL} />
-                    <div className="QSP_wrapper" >
-                        <input type={"image"} name="Q" src={iconQ} />
-                        <input type={"image"} name="S" src={iconS} />
-                        <input type={"image"} name="P" src={iconP} />
+        <>
+            <div className="SideNav">
+                <nav>
+                    <div className="nav_wrapper">
+                        <div className="icon_wrapper" >
+                            <input type={"image"} name="MBL" src={iconMBL} />
+                            <input type={"image"} name="Q" src={iconQ} 
+                                onClick={handleClassNameQ} />
+                            <input type={"image"} name="S" src={iconS} 
+                                onClick={handleClassNameS} />
+                            <input type={"image"} name="P" src={iconP} 
+                                onClick={handleClassNameP} />
+                        </div>
                     </div>
+                    <input id="setting" type={"image"} name=">" src={icon_} />
+                </nav>
+            </div>
+            <div className="QnA">
+                <p ref={ref_title_QnA} className='title'>Q&A</p>
+                <div ref={ref_container_QnA} className='container'>
                 </div>
-                <input id="setting" type={"image"} name=">" src={icon_} />
-            </nav>
-        </div>
+            </div>
+            <div className="Study">
+                <p ref={ref_title_Study} className='title'>STUDY</p>
+                <div ref={ref_container_Study} className='container'>
+                </div>
+            </div>
+            <div className="Project">
+                <p ref={ref_title_Project} className='title'>PROJECT</p>
+                <div ref={ref_container_Project} className='container'>
+                </div>
+            </div>
+        </>
     );
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useRef ,useEffect} from 'react';
 import Banner from '../components/Banner';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import SideNav from '../components/SideNav';
 import ThemecastContent from '../components/ThemecastContent';
 
 export default function Main (){
+    
+
     return(
-        <div className="Main">
-            <Header/>
+        <div  className="Main">
+            {/* <Header/>
             <Banner/>
             <ThemecastContent />
-            <Footer/>
+            <Footer/> */}
+            <SideNav/>
         </div>
     );
 }
